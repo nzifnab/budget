4ed9cb252e961fd49d07ae5d550836589eb2fbf1
Nathan Benes <nathan.benes@gmail.com>
2016-03-19 19:23:12 -0600

    precompile assets

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,8 @@ module Budgeteer
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Mountain Time (US & Canada)'
+    config.encoding = "utf-8"
+    config.filter_parameters += [:password, :password_confirmation]
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
@@ -35,5 +37,9 @@ module Budgeteer
 
     # Generate structure.sql instead of schema.rb for DB schema.
     config.active_record.schema_format = :sql
+
+    config.assets.precompile += %w(
+      screen.css
+    )
   end
 end
